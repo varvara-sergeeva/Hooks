110c8a934d770ad3efac8fadbd5d8766eb6591b8
varvara-sergeeva <raunuperetemheru@gmail.com>
2023-06-04 15:44:57 +0300

    add default

---
+++ src/examples/defaultApp.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import React from "react";
+
+function App() {
+	return ( <
+		div className = "wrap" >
+		<
+		h1 > Разбор хуков и их применения < /h1> <
+		/div>
+	);
+}
+
+export default App;
