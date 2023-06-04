538c0f09025c8ee8c431c88536189de466327df4
varvara-sergeeva <raunuperetemheru@gmail.com>
2023-06-04 15:45:29 +0300

    fix default

--- src/examples/defaultApp.js
+++ src/examples/defaultApp.js
@@ -3,12 +3,11 @@ import "./App.css";
 import React from "react";
 
 function App() {
-	return ( <
-		div className = "wrap" >
-		<
-		h1 > Разбор хуков и их применения < /h1> <
-		/div>
-	);
+  return (
+    <div className="wrap">
+      <h1>Разбор хуков и их применения</h1>
+    </div>
+  );
 }
 
 export default App;
